d1f9113f2c01a7ce7cd1ecabb574cbe2cd237ba3
syed zainul abedin <szsimplesyed93@gmail.com>
2017-04-12 01:04:59 +0530

    Reformated code

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import flask,render_template,request,redirect
-
 app=Flask(__name__)
 
 @app.route('/')
 def main():
 	return "Alllrigghtty then"
 
-if __name__=='__main__'
+if __name__=='__main__':
 	app.run(debug=true)
