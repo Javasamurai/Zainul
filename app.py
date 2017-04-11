ceabbd546cd82da01a1611ae1bd1237e7d397b0c
syed zainul abedin <szsimplesyed93@gmail.com>
2017-04-12 00:43:33 +0530

    Added requirement py files and Procfile

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import flask,render_template,request,redirect
+
+app=Flask(__name__)
+
+@app.route('/')
+def main():
+	return "Alllrigghtty then"
+
+if __name__=='__main__'
+	app.run(debug=true)
