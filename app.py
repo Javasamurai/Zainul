f31cf6bdb8bce9cace012b92de35a2ff50755215
syed zainul abedin <szsimplesyed93@gmail.com>
2017-04-20 17:09:38 +0530

    Added moment library

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask,render_template,request,redirect
+import time
 app=Flask(__name__)
 
 @app.route('/')
 def main():
 	return render_template("index.html")
 
+@app.route('/ramadan_timer')
+def timer():
+	return render_template("ramadan_timer.html")
+
 if __name__=='__main__':
-	app.run(debug=true)
+	app.run(debug=True)
